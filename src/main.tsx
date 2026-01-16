b2e142d8f1dd8b57d0466cd141bfb2d648e170a1
abidi-muhammad <abidimuhammad345@gmail.com>
2026-01-16 23:16:45 +0500

    Deploy to root domain

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename="/qiam">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>,
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 export default defineConfig({
-  base: '/qiam/',  // ← IMPORTANT! Your subdirectory
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
